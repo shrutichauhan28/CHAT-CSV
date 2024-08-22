af36ff16bda19b25c98bc3ed176bc8a80338c5d8
shrutichauhan28 <87888938+shrutichauhan28@users.noreply.github.com>
2024-08-22 17:17:01 +0530

    changed the model

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ if not api_key:
     st.info("Please add the Groq API key")
 
 # Initialize the Groq LLM
-llm = ChatGroq(groq_api_key=api_key, model_name="Llama3-8b-8192", streaming=True)
+llm = ChatGroq(groq_api_key=api_key, model_name="gemma2-9b-it", streaming=True)
 
 # Function to configure SQLite database
 @st.cache_resource(ttl="2h")
